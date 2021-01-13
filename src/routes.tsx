031da19068228e7609851b164129f74797c45cb2
Corete <fanthell95@gmail.com>
2021-01-13 20:59:04 +0900

    초기세팅 라우터 연결

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, COLORS } from "./styles";
+import mainPage from "./pages/mainPage";
+import aboutPage from "./pages/aboutPage";
+import alcoholDetail from "./pages/alcoholDetail";
+import featherDetail from "./pages/featherDetail";
+import kobbubakDetail from "./pages/kobbubakDetail";
+import metorDetail from "./pages/metorDetail";
+import storeDetail from "./pages/storeDetail";
+import storeList from "./pages/storeList";
+import themeList from "./pages/themeList";
+import todayRandom from "./pages/todayRandom";
+import weMeokTalkMain from "./pages/weMeokTalkMain";
+import weMeokTalkDetail from "./pages/weMeokTalkDetail";
 
 class Routes extends React.Component {
   render() {
@@ -11,7 +23,22 @@ class Routes extends React.Component {
           <GlobalStyle />
           <Router>
             <Switch>
-              <Route exact path="/" component={} />
+              <Route exact path="/" component={mainPage} />
+              <Route exact path="/aboutPage" component={aboutPage} />
+              <Route exact path="/alcoholDetail" component={alcoholDetail} />
+              <Route exact path="/featherDetail" component={featherDetail} />
+              <Route exact path="/kobbubakDetail" component={kobbubakDetail} />
+              <Route exact path="/metorDetail" component={metorDetail} />
+              <Route exact path="/storeDetail" component={storeDetail} />
+              <Route exact path="/storeList" component={storeList} />
+              <Route exact path="/themeList" component={themeList} />
+              <Route exact path="/todayRandom" component={todayRandom} />
+              <Route exact path="/weMeokTalkMain" component={weMeokTalkMain} />
+              <Route
+                exact
+                path="/weMeokTalkDetail"
+                component={weMeokTalkDetail}
+              />
             </Switch>
           </Router>
         </ThemeProvider>
